295d32f17849c2c8865ffbf8ed4d9c8f1988ae35
Nathan <25828721+NathanCollaert@users.noreply.github.com>
2026-01-13 09:59:19 +0100

    feat: slot Skruuia when popping all wrinklers

--- src/game/actions/popWrinklers/PopWrinklersAction.ts
+++ src/game/actions/popWrinklers/PopWrinklersAction.ts
@@ -1,18 +1,18 @@
 import { Config } from "../../../Config";
 import { game } from "../../../index";
 import { LogEngine } from "../../../logging/LogEngine";
-import { formatNumber } from "../../../utils/Formatting";
 import { HALLOWEEN_COOKIES, isSeasonActive, unlockedAllUpgrades } from "../../../utils/Game";
+import { PantheonEngine } from "../../pantheon/PantheonEngine";
+import { God } from "../../types/God";
 import { LogType } from "../../types/LogType";
+import { PantheonSlot } from "../../types/PantheonSlot";
 import { Season } from "../../types/Season";
-import { Wrinkler } from "../../types/Wrinkler";
 import { Action } from "../Action";
 
-export class popWrinklerAction extends Action {
+export class popWrinklersAction extends Action {
     public enabled: boolean = true;
-    public interval: number = Config.popWrinklerInterval;
+    public interval: number = Config.intervals.popWrinklerInterval;
     public shouldExecuteImmediately: boolean = true;
-    private wrinkler: Wrinkler | null = null;
 
     protected canExecute(): boolean {
         if (game.Upgrades["One mind"].bought === 0) return false;
@@ -24,29 +24,14 @@ export class popWrinklerAction extends Action {
     }
 
     protected executeAction(): boolean {
-        this.wrinkler = this.getOldestWrinkler();
-        if (!this.wrinkler) return false;
-
-        this.wrinkler.hp = 0;
-
+        PantheonEngine.instance.setManualGod(God.SKRUUIA, PantheonSlot.DIAMOND);
+        game.CollectWrinklers();
+        PantheonEngine.instance.clearManualGod();
         return true;
     }
 
     protected log(): void {
-        // TODO: sometimes logs 5 times
-        if (this.wrinkler)
-            LogEngine.addLog(LogType.WRINKLER, `Popped wrinkler | ${formatNumber(this.wrinkler.sucked)}`)
-    }
-
-    protected finalize(): void {
-        this.wrinkler = null;
-    }
-
-    private getOldestWrinkler(): Wrinkler | null {
-        return game.wrinklers.reduce<Wrinkler | null>((max, item) => {
-            if (item.close !== 1) return max
-            if (!max || item.sucked > max.sucked) return item
-            return max
-        }, null);
+        // TODO - FIX: sometimes logs 5 times
+        LogEngine.addLog(LogType.WRINKLER, `Popped all wrinklers`)
     }
 }
